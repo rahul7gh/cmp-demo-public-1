436102fa281c549cb0e5fb17449601987274ae80
rahul7gh <30512797+rahul7gh@users.noreply.github.com>
2025-05-23 20:08:02 +0530

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,12 +1,24 @@
 import argparse
 import json
+import requests
 parser = argparse.ArgumentParser(description="Process extra vars")
 parser.add_argument("--ipaddress", type=str, help="ipaddress")
 parser.add_argument("--machine_name", type=str, help="machine_name")
 
 args = parser.parse_args()
 output={"ipaddress":args.ipaddress,"machine_name":args.machine_name,"cpu":"16","memory":"16384MB","disk":"131072MB"}
-print(json.dumps({"output":output}))
+
+url = "https://api.ipify.org?format=json"
+try:
+    response = requests.get(url)
+    response.raise_for_status()  # Raise exception for HTTP errors
+    data = response.json()
+    print({"output": {"ip": data["ip"]}})
+except requests.RequestException as e:
+    print({"output": {"error": "An error occurred:"+ str(e)}})
+    
+
+# print(json.dumps({"output":output}))
 
 
 #python3 test.py --ipaddress=10.25.10.20 --machine_name=YOSH
